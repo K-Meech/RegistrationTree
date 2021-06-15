5e0b6bf5ec30b0b194e24bac99fa075eb57b02bb
Kimberly Meechan <kimberly.meechan@embl.de>
2021-06-15 12:46:54 +0100

    provide size estimates

--- src/main/java/de/embl/schwab/crosshairSBEM/Downsampler.java
+++ src/main/java/de/embl/schwab/crosshairSBEM/Downsampler.java
@@ -8,19 +8,45 @@ public class Downsampler {
 
     public Downsampler( Transformer transformer ) { this.transformer = transformer; }
 
+    private double getEstimatedSizeInGigaBytes( long[] voxelDimensions ) {
+        long nVoxels = 1;
+        for ( long voxelDim: voxelDimensions ) {
+            nVoxels = nVoxels * voxelDim;
+        }
+
+        // assume 8-bit. We currently write all elastix images in 8-bit
+        // then one uncompressed voxel == 1 byte
+
+        // round to 3 dp
+        double sizeEstimate = (double) nVoxels / 1000000000.0;
+        sizeEstimate = Math.round(sizeEstimate * 1000.0) / 1000.0;
+        return sizeEstimate;
+    }
+
+    private String makeResolutionString( int level, long[] sourceDimensions, double sizeEstimate ) {
+        StringBuilder resolutionLevelString = new StringBuilder( Integer.toString(level) );
+        resolutionLevelString.append("  |  ");
+        for ( int j = 0; j<sourceDimensions.length; j++) {
+            if ( j != 0 ) {
+                resolutionLevelString.append("-");
+            }
+            resolutionLevelString.append( sourceDimensions[j] );
+        }
+
+        resolutionLevelString.append("  |  ");
+        resolutionLevelString.append( sizeEstimate ).append( "GB");
+
+        return resolutionLevelString.toString();
+    }
+
     public String[] getLevelsArray( Transformer.ImageType imageType ) {
         int numLevels = transformer.getSourceNumberOfLevels( imageType );
 
         String[] resolutionLevels = new String[numLevels];
         for ( int i = 0; i < numLevels; i++ ) {
             long[] sourceDimensions = transformer.getSourceVoxelDimensions( imageType, i );
-
-            StringBuilder resolutionLevelString = new StringBuilder( Integer.toString(i) );
-            for ( int j = 0; j<sourceDimensions.length; j++) {
-                resolutionLevelString.append("-").append( sourceDimensions[j] );
-            }
-
-            resolutionLevels[i] = resolutionLevelString.toString();
+            double sizeEstimate = getEstimatedSizeInGigaBytes( sourceDimensions );
+            resolutionLevels[i] = makeResolutionString( i, sourceDimensions, sizeEstimate );
         }
 
         return resolutionLevels;
@@ -32,13 +58,9 @@ public class Downsampler {
         String[] resolutionLevels = new String[numLevels];
         for ( int i = 0; i < numLevels; i++ ) {
             Interval interval = transformer.getCropper().getImageCropIntervalVoxelSpace( imageType, cropName, i );
+            double sizeEstimate = getEstimatedSizeInGigaBytes( interval.dimensionsAsLongArray() );
 
-            StringBuilder resolutionLevelString = new StringBuilder( Integer.toString(i) );
-            for ( int j = 0; j<interval.numDimensions(); j++) {
-                resolutionLevelString.append("-").append(interval.dimension(j));
-            }
-
-            resolutionLevels[i] = resolutionLevelString.toString();
+            resolutionLevels[i] = makeResolutionString( i, interval.dimensionsAsLongArray(), sizeEstimate );
         }
 
         return resolutionLevels;
