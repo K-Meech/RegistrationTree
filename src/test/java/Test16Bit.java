16564b16be2fd65fc45fc518c53becd797aee954
Kimberly Meechan <kimberly.meechan@embl.de>
2021-11-16 13:50:34 +0100

    add images for wiki

--- src/test/java/Test16Bit.java
+++ src/test/java/Test16Bit.java
@@ -6,7 +6,7 @@ public class Test16Bit {
     public static void main( String[] args ) {
         RegistererCommand command = new RegistererCommand();
         command.fixedImageXml = new File("C:\\Users\\meechan\\Documents\\temp\\test_sbem\\test_n5\\first_100_proper_z_spacing\\ov000_0.xml");
-        command.movingImageXml = new File("C:\\Users\\meechan\\Documents\\test_images\\sbem_run\\x-ray\\Platy-89_02_tomo.xml");
+        command.movingImageXml = new File("C:\\Users\\meechan\\Documents\\test_images\\sbem_run\\x-ray\\Platy-89_02_tomo-flippedZ.xml");
         command.temporaryDirectory = new File( "C:\\Users\\meechan\\Documents\\temp\\crosshairElastixTesting" );
         command.run();
     }
